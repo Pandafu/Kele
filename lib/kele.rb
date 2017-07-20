823434d184844d2d161f938ac73b6888f6425eb3
Riss <rissgadi@gmail.com>
2017-07-19 21:29:57 -0500

    completed checkpoint3

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 #require './lib/kele'
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  #attr_reader :auth_token
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
@@ -14,5 +16,11 @@ class Kele
     }
 
     @auth = self.class.post("/sessions", options)
+    #puts @auth
+    @auth_token = @auth["auth_token"] 
   end
+
+  def get_me
+      response = JSON.parse(self.class.get("/users/me", headers: { "Authorization" => @auth_token }).body)
+    end
 end
