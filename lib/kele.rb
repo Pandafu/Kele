eb748fc05f6c151befee7725251a2747d162c82c
Riss <rissgadi@gmail.com>
2017-07-22 11:38:40 -0500

    completed checkpoint7

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,7 +9,7 @@ class Kele
   include HTTParty
   include Roadmap
   include Message
-  include Mentor 
+  include Mentor
   #attr_reader :auth_token
   base_uri 'https://www.bloc.io/api/v1'
 
@@ -29,4 +29,14 @@ class Kele
   def get_me
       response = JSON.parse(self.class.get("/users/me", headers: { "Authorization" => @auth_token }).body)
     end
+
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment)
+    create_message = {
+      "assignment_branch": assignment_branch.to_str,
+      "assignment_commit_link": assignment_commit_link.to_str,
+      "checkpoint_id": checkpoint_id.to_str,
+      "comment": comment.to_str
+    }
+    response = JSON.parse(self.class.post("/checkpoint_submissions", headers: { "Authorization" => @auth_token }, :body => create_message).body)
+  end
 end
